2023773d4aa5e783bebc3ac07884cbeeaf870581
Wael Elkadim <wael.elkadim@microsoft.com>
2024-08-11 14:09:37 +0300

    Add API Project and add context

---
+++ WebApplicationAPI/Entities/VEmployeeDepartmentHistory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplicationAPI.Entities;
+
+public partial class VEmployeeDepartmentHistory
+{
+    public int BusinessEntityId { get; set; }
+
+    public string? Title { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string? MiddleName { get; set; }
+
+    public string LastName { get; set; } = null!;
+
+    public string? Suffix { get; set; }
+
+    public string Shift { get; set; } = null!;
+
+    public string Department { get; set; } = null!;
+
+    public string GroupName { get; set; } = null!;
+
+    public DateOnly StartDate { get; set; }
+
+    public DateOnly? EndDate { get; set; }
+}
